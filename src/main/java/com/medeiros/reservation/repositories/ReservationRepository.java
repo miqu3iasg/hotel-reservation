653d58006f838f376d686dd3a52d472207b5115f
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-03-09 19:30:48 -0300

    feat: add reservation repository

--- src/main/java/com/medeiros/reservation/repositories/ReservationRepository.java
+++ src/main/java/com/medeiros/reservation/repositories/ReservationRepository.java
@@ -1,4 +1,31 @@
 package com.medeiros.reservation.repositories;
 
-public interface ReservationRepository {
+import com.medeiros.reservation.entities.customer.Customer;
+import com.medeiros.reservation.entities.reservation.Reservation;
+import com.medeiros.reservation.entities.room.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+  Reservation findReservationByCustomer(Customer customer);
+
+  List<Reservation> findReservationsByRoomAndCheckOutAfterAndCheckInBefore (
+          Room room,
+          LocalDateTime checkIn,
+          LocalDateTime checkOut
+  );
+
+  List<Reservation> findReservationsByRoomAndCheckInBetweenOrCheckOutBetween (
+          Room room,
+          LocalDateTime checkInStart,
+          LocalDateTime checkInEnd,
+          LocalDateTime checkOutStart,
+          LocalDateTime checkOutEnd
+  );
 }
+
