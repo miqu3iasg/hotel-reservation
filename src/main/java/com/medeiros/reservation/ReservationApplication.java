6b77b69318b02d0610b2fcc1f28edf477949a853
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-03-09 19:12:16 -0300

    feat: add initial config

--- src/main/java/com/medeiros/reservation/ReservationApplication.java
+++ src/main/java/com/medeiros/reservation/ReservationApplication.java
@@ -2,8 +2,12 @@ package com.medeiros.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.medeiros.reservation")
+@EntityScan(basePackages = "com.medeiros.reservation.entities")
 public class ReservationApplication {
 
 	public static void main(String[] args) {
