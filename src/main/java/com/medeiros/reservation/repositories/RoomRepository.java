83f007181e11b721ad17b2071f8cc52e6ba9bbe8
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-03-09 19:31:09 -0300

    feat: add room repository

--- src/main/java/com/medeiros/reservation/repositories/RoomRepository.java
+++ src/main/java/com/medeiros/reservation/repositories/RoomRepository.java
@@ -1,2 +1,13 @@
-package com.medeiros.reservation.repositories;public interface RoomRepository {
+package com.medeiros.reservation.repositories;
+
+import com.medeiros.reservation.entities.room.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, UUID> {
+  Optional<Room> findByNumber(int number);
 }
