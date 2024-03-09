bc227b932c68a9424d8fa7bba53f8f1145696f70
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-03-09 19:30:15 -0300

    feat: add hotel repository

--- src/main/java/com/medeiros/reservation/repositories/HotelRepository.java
+++ src/main/java/com/medeiros/reservation/repositories/HotelRepository.java
@@ -1,2 +1,12 @@
-package com.medeiros.reservation.repositories;public interface HotelRepository {
+package com.medeiros.reservation.repositories;
+
+import com.medeiros.reservation.entities.hotel.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface HotelRepository extends JpaRepository<Hotel, UUID> {
+  Hotel findByName(String name);
 }
